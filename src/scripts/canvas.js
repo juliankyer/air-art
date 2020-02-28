4ab421df3aad430e0f2b7f019e606fe6ac4f57eb
Trevor DiMartino <trevor.dimartino@cognizant.com>
2020-02-27 17:10:44 -0700

    I did something.

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,7 +1,5 @@
-export function drawLines(canvas, context) {
+export function drawLines(canvas, context, fake_data) {
     const base_radius = 2;
-
-    const fake_data = {"location": [0.8553116321563721, 0.923753559589386], "speed": 0.2241044044494629, "segments": [{"unit_direction": [-0.018715707420810992, -0.9998248458083738], "acceleration": 0.9780861735343933}, {"unit_direction": [-0.13321027927612328, -0.9910877970670294], "acceleration": 0.9903990626335144}, {"unit_direction": [0.21278553171237005, -0.9770989292256358], "acceleration": 0.9926618337631226}, {"unit_direction": [0.3787067026989507, -0.9255167385471149], "acceleration": 1.0186649560928345}, {"unit_direction": [0.7733454372270593, -0.6339848852457671], "acceleration": 1.0016921758651733}, {"unit_direction": [0.7259171792724889, -0.6877821230862817], "acceleration": 1.0008735656738281}, {"unit_direction": [0.6380155855197716, -0.7700234494051873], "acceleration": 1.0234256982803345}, {"unit_direction": [0.44969184111156674, -0.8931837705857006], "acceleration": 1.0090147256851196}]};
 
     const [start_x, start_y] = fake_data.location;
 
@@ -47,6 +45,7 @@ export function drawLines(canvas, context) {
         pen_ball.acceleration = segment.acceleration;
         pen_ball.x_direction = segment.unit_direction[0];
         pen_ball.y_direction = segment.unit_direction[1];
+        pen_ball.radius = base_radius - (base_radius * pen_ball.speed);
         
         window.requestAnimationFrame(draw);
         segment_index += 1;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 import '../styles/index.scss';
 import { drawLines } from './canvas.js';
 
+const fake_data = require('../data/fake_data.json');
+
 function docReady(fn) {
   if (document.readyState === "complete" || document.readyState === "interactive") {
       setTimeout(fn, 1);
@@ -13,5 +15,5 @@ docReady(function() {
   const canvas = document.getElementById('canvas');
   const context = canvas.getContext('2d');
 
-  drawLines(canvas, context);
+  fake_data.forEach(data => drawLines(canvas, context, data));
 });
